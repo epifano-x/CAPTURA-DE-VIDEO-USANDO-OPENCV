6de441f8a38b827cb586cd4dc75d6fe508e13e0d
BRUNO ROGLIN <49683237+epifano-x@users.noreply.github.com>
2023-08-31 23:05:20 -0300

    stable
    media e trash funcional

--- captura_imagem.py
+++ captura_imagem.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
+import cv2
+import numpy as np
 
 # Função para atualizar as coordenadas
 def atualizar_coordenadas(event):
@@ -11,7 +13,7 @@ def atualizar_coordenadas(event):
 # Função para mostrar a cor RGB e atualizar a imagem editada
 def mostrar_cor_rgb(event):
     x, y = event.x, event.y
-    pixel = imagem_capturada.getpixel((x, y))
+    pixel = imagem_capturada[y, x]  # Obtenha o pixel da imagem capturada
     cor_rgb = f"RGB: {pixel[0]}, {pixel[1]}, {pixel[2]}"
     
     # Atualizar a label com as coordenadas
@@ -21,11 +23,32 @@ def mostrar_cor_rgb(event):
     rgb_label.config(text=cor_rgb)
 
     # Atualizar a cor do quadrado
-    quadrado_cor.config(bg=f"#{pixel[0]:02X}{pixel[1]:02X}{pixel[2]:02X}")
+    quadrado_cor_label.config(bg=f"#{pixel[0]:02X}{pixel[1]:02X}{pixel[2]:02X}")
 
     # Aplicar a cor na imagem editada
-    imagem_editada.paste((pixel[0], pixel[1], pixel[2]), (x, y, x + 1, y + 1))
-    img_editada = ImageTk.PhotoImage(imagem_editada)
+    imagem_editada[y, x] = pixel
+    img_editada = ImageTk.PhotoImage(Image.fromarray(imagem_editada))
+    label_imagem_editada.config(image=img_editada)
+    label_imagem_editada.image = img_editada
+
+# Função para aplicar o filtro passa-baixa média na imagem editada
+def aplicar_filtro_media(val):
+    valor = int(float(val))
+    kernel = np.ones((valor, valor), dtype=np.float32) / (valor * valor)
+    img_filtrada = cv2.filter2D(imagem_editada, -1, kernel)
+    imagem_editada_cv2[:] = img_filtrada[:]
+
+    img_editada = ImageTk.PhotoImage(Image.fromarray(imagem_editada_cv2))
+    label_imagem_editada.config(image=img_editada)
+    label_imagem_editada.image = img_editada
+
+# Função para aplicar o filtro de threshold na imagem editada
+def aplicar_filtro_threshold(val):
+    valor = int(float(val))
+    _, img_filtrada = cv2.threshold(imagem_editada, valor, 255, cv2.THRESH_BINARY)
+    imagem_editada_cv2[:] = img_filtrada[:]
+
+    img_editada = ImageTk.PhotoImage(Image.fromarray(imagem_editada_cv2))
     label_imagem_editada.config(image=img_editada)
     label_imagem_editada.image = img_editada
 
@@ -33,7 +56,8 @@ def mostrar_cor_rgb(event):
 imagem_capturada = Image.open("imagem_capturada_webcam.jpg")
 
 # Criar uma cópia da imagem capturada para a imagem editada
-imagem_editada = imagem_capturada.copy()
+imagem_editada = np.array(imagem_capturada)
+imagem_editada_cv2 = imagem_editada.copy()
 
 # Configurar a interface gráfica
 root = tk.Tk()
@@ -65,7 +89,7 @@ quadrado_frame = ttk.Frame(frame_direita, width=20, height=20)
 quadrado_frame.pack(side="top")
 
 # Configurar um Label para a imagem editada
-img_editada = ImageTk.PhotoImage(imagem_editada)
+img_editada = ImageTk.PhotoImage(Image.fromarray(imagem_editada))
 label_imagem_editada = ttk.Label(frame_imagem, image=img_editada)
 label_imagem_editada.pack(side="top")
 
@@ -74,5 +98,21 @@ label_imagem.bind("<Motion>", atualizar_coordenadas)
 
 # Lidar com eventos de clique do mouse na imagem
 label_imagem.bind("<Button-1>", mostrar_cor_rgb)
+
+# Criar um Frame para as Trackbars
+frame_trackbar = ttk.Frame(frame_direita)
+frame_trackbar.pack(side="top")
+
+# Trackbar para filtro passa-baixa média
+ttk.Label(frame_trackbar, text="Filtro Média").pack(side="top")
+filtro_media_var = tk.StringVar()
+filtro_media_var.set("1")  # Valor inicial
+ttk.Scale(frame_trackbar, from_=1, to=20, variable=filtro_media_var, command=aplicar_filtro_media).pack(side="top")
+
+# Trackbar para filtro de threshold
+ttk.Label(frame_trackbar, text="Filtro Threshold").pack(side="top")
+filtro_threshold_var = tk.StringVar()
+filtro_threshold_var.set("1")  # Valor inicial
+ttk.Scale(frame_trackbar, from_=1, to=255, variable=filtro_threshold_var, command=aplicar_filtro_threshold).pack(side="top")
 
 root.mainloop()
